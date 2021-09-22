f01324769b97caa2358e22f043b74a4a80a97c6b
DanilZen <a64danil@mail.ru>
2021-09-22 20:19:20 +0300

    add logs for DZ1. Dz1 is done

--- dz1/init.ts
+++ dz1/init.ts
@@ -4,9 +4,20 @@ const myCurrency = new Currency(200.44);
 
 console.log(myCurrency)
 console.log(myCurrency.dollars())
-console.log(myCurrency.cents())
-console.log('-------------------')
-console.log(myCurrency.add(10).dollars())
-console.log(myCurrency.format(true))
-console.log(myCurrency.toString())
-console.log(myCurrency.toJSON())
+// // console.log(myCurrency.cents());
+// console.log('-------------------');
+// const item = myCurrency.add(10);
+// console.log(item.dollars());
+// console.log(myCurrency.format(true));
+// // console.log(myCurrency.toString());
+// // console.log(myCurrency.toJSON());
+// console.log(myCurrency.dollars());
+//
+// console.log('-------------------');
+// console.log(item.add(23));
+// console.log(item.dollars());
+// console.log(myCurrency.add(20) === myCurrency); // false даёт
+console.log('distribute', myCurrency.distribute(15))
+console.log('divide', myCurrency.divide(10))
+console.log('subtract', myCurrency.subtract(10))
+console.log('multipl', myCurrency.multiply(10))
